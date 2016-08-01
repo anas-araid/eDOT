b4c2bac6e07190a94b9eb4483ad5d4895bf088cb
masinag <gabriele.masina@virgilio.it>
2016-08-01 09:16:55 +0200

    Add validations to patients

--- Gemfile
+++ Gemfile
@@ -5,6 +5,11 @@ source 'https://rubygems.org'
 gem 'rails', '~> 5.0.0'
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3'
+
+gem 'geocoder'
+
+gem 'validates_phone_number'
+
 # Use Puma as the app server
 gem 'puma', '~> 3.0'
 # Use SCSS for stylesheets
--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,2 +1,14 @@
 class Patient < ApplicationRecord
+  validates :name, :surname, :gender, :birthdate, presence:true
+  validates :phone, :phone_number => {:format => /^[\d-]*$/}
+
+  validate :valid_address
+
+  def valid_address
+    if Geocoder.search(address).empty?
+      errors.add(:address, "doesn't seem to be a valid address")
+    end
+    !Geocoder.search(address).empty?
+
+  end
 end
